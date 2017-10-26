6a14cb979bcde8f22c933c22bafffd79282b662d
Joseph <cutinoj@mail.gvsu.edu>
2017-10-26 10:02:37 -0400

    Removed not on thursdays

--- Main.py
+++ Main.py
@@ -134,7 +134,7 @@ def notify_logic(connection_status, listProcessResult, contact_eng, outOfDate, l
             print("List Day: ", date_l)
             print("Current Day: ", date.day)
         '''Send list update recommendations on thrusday, friday, and saturday'''
-        if((date_l-date.day) in range(2,5)):
+        if((date_l-date.day) in range(2,4)):
             subject_text = """Time to review next weeks shopping list.\nCheck your email for recommendations."""
             #Notify individuals that the list should be looked at.
             '''contact_eng.notify_Employee(text_file = RECOMMENDATIONS_FILE_NAME,
